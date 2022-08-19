392557ad649106de6aaa6bf666fbb016ebf15bb6
Cyb3rh4ck <gigabyte.mx@gmail.com>
2022-08-19 10:18:21 -0700

    new class

---
+++ JavaPatterns/src/com/java/pattern/abstract_factory/example/buttons/MacOSButton.java
@@ -0,0 +1,14 @@
+package com.java.pattern.abstract_factory.example.buttons;
+
+/**
+ * 
+ * @author Cyb3rh4ck
+ * All proudcts families have the same varieties (MacOS/Windows).
+ *
+ * This is a MacOS variant of a button
+ *
+ */
+
+public class MacOSButton {
+
+}
